d691065155d204db2217a7d5828e4061274adffe
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-26 19:03:03 +0900

    P03 の準備

--- src/main/scala/problems/Problems01.scala
+++ src/main/scala/problems/Problems01.scala
@@ -42,4 +42,14 @@ object WorkingWithLists {
     case x :: _ :: Nil => x
     case x :: xs => penultimate(xs)
   }
+
+  /**
+   * P03 (*) Find the Kth element of a list.
+   * By convention, the first element in the list is element 0.
+   *
+   * Example:
+   *   scala> nth(2, List(1, 1, 2, 3, 5, 8))
+   *   res0: Int = 2
+   */
+  def nth[A](n: Int, list: List[A]): A = ???
 }
--- src/test/scala/problems/Problems01Spec.scala
+++ src/test/scala/problems/Problems01Spec.scala
@@ -28,5 +28,27 @@ class WorkingWithListsSpec extends Specification {
       penultimate(List(1, 1, 2, 3, 5, 8)) must beEqualTo(5)
     }
   }
+
+  "nth(Int, List[A])" should {
+    "throws IndexOutOfBoundsException if (n, list) = (-1, [1, 1, 2, 3, 5, 8])" in {
+      nth(-1, List(1, 1, 2, 3, 5, 8)) must throwA[IndexOutOfBoundsException]
+    }
+
+    "throws 1 if (n, list) = (0, [1, 1, 2, 3, 5, 8])" in {
+      nth(0, List(1, 1, 2, 3, 5, 8)) must beEqualTo(1)
+    }
+
+    "throws 2 if (n, list) = (2, [1, 1, 2, 3, 5, 8])" in {
+      nth(2, List(1, 1, 2, 3, 5, 8)) must beEqualTo(2)
+    }
+
+    "throws IndexOutOfBoundsException if (n, list) = (0, [])" in {
+      nth[String](0, Nil) must throwA[IndexOutOfBoundsException]
+    }
+
+    "throws IndexOutOfBoundsException if (n, list) = (6, [1, 1, 2, 3, 5, 8])" in {
+      nth(6, List(1, 1, 2, 3, 5, 8)) must throwA[IndexOutOfBoundsException]
+    }
+  }
 }
 
