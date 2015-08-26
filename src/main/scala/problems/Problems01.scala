773ad8e60c7827260bcce021a1bfbc5bac0d1b09
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-26 18:47:36 +0900

    P01 の準備

---
+++ src/main/scala/problems/Problems01.scala
@@ -0,0 +1,23 @@
+package problems
+
+object WorkingWithLists {
+  // [テンプレート]
+  //
+  // /**
+  //  * P01 (*) Find the last element of a list.
+  //  *
+  //  * Example:
+  //  *   scala> last(List(1, 1, 2, 3, 5, 8))
+  //  *   res0: Int = 8
+  //  */
+  // def last[A](list: List[A]): A = ???
+
+  /**
+   * P01 (*) Find the last element of a list.
+   *
+   * Example:
+   *   scala> last(List(1, 1, 2, 3, 5, 8))
+   *   res0: Int = 8
+   */
+  def last[A](list: List[A]): A = ???
+}
---
+++ src/test/scala/problems/Problems01Spec.scala
@@ -0,0 +1,18 @@
+package test.problems
+
+import problems.WorkingWithLists._
+import org.specs2.mutable._
+import java.util.NoSuchElementException
+
+class WorkingWithListsSpec extends Specification {
+  "last(List[A])" should {
+    "throws NoSuchElementException if list = []" in {
+      last[Int](Nil) must throwA[NoSuchElementException]
+    }
+
+    "returns 8 if list = [1, 1, 2, 3, 5, 8]" in {
+      last(List(1, 1, 2, 3, 5, 8)) must beEqualTo(8)
+    }
+  }
+}
+
