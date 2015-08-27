7ddd7b191342e157870364da66d8a4983a0d281d
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-27 20:16:29 +0900

    P31 の準備

--- src/main/scala/problems/Problems02.scala
+++ src/main/scala/problems/Problems02.scala
@@ -3,6 +3,25 @@ package problems
 package arithmetic {
   class S99Int(val start: Int) {
     import S99Int._
+
+    // [テンプレート]
+    //
+    // /**
+    //  * P01 (*) Find the last element of a list.
+    //  *
+    //  * Example:
+    //  *   scala> last(List(1, 1, 2, 3, 5, 8))
+    //  *   res0: Int = 8
+    //  */
+    // def last[A](list: List[A]): A = ???
+
+    /**
+     * P31 (**) Determine whether a given integer number is prime.
+     *
+     * scala> 7.isPrime
+     * res0: Boolean = true
+     */
+    def isPrime: Boolean = ???
   }
 
   object S99Int {
--- src/test/scala/problems/Problems02Spec.scala
+++ src/test/scala/problems/Problems02Spec.scala
@@ -6,4 +6,29 @@ import problems.arithmetic._
 import org.specs2.mutable._
 
 class ArithmeticSpec extends Specification {
+  "S99Int#isPrime" should {
+    "returns true if 2" in {
+      new S99Int(2).isPrime must beTrue
+    }
+
+    "returns true if 3" in {
+      new S99Int(3).isPrime must beTrue
+    }
+
+    "returns false if 4" in {
+      new S99Int(4).isPrime must beFalse
+    }
+
+    "returns true if 5" in {
+      new S99Int(5).isPrime must beTrue
+    }
+
+    "returns false if 6" in {
+      new S99Int(6).isPrime must beFalse
+    }
+
+    "returns true if 7" in {
+      new S99Int(7).isPrime must beTrue
+    }
+  }
 }
