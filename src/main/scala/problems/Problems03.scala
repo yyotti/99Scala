8a36f202da3c2842fbc74e28634590172110656f
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-28 12:32:14 +0900

    Logic and Codes 章の準備

---
+++ src/main/scala/problems/Problems03.scala
@@ -0,0 +1,13 @@
+package problems.logic
+
+object S99Logic {
+  // [テンプレート]
+  //
+  // /**
+  //  * P31 (**) Determine whether a given integer number is prime.
+  //  *
+  //  * scala> 7.isPrime
+  //  * res0: Boolean = true
+  //  */
+  // def isPrime: Boolean = ???
+}
---
+++ src/test/scala/problems/Problems03Spec.scala
@@ -0,0 +1,12 @@
+package test.problems
+
+import problems.logic._
+import org.specs2.mutable._
+
+class LogicAndCodesSpec extends Specification {
+  "S99Logic#example" should {
+    "???" in {
+      pending
+    }
+  }
+}
