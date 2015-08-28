f760527a8bc5fbe1bac3980485bd438a2813c8d0
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-28 12:52:51 +0900

    P46 の準備

--- src/main/scala/problems/Problems03.scala
+++ src/main/scala/problems/Problems03.scala
@@ -1,6 +1,8 @@
 package problems.logic
 
 object S99Logic {
+  import java.io.OutputStream
+
   // [テンプレート]
   //
   // /**
@@ -10,4 +12,37 @@ object S99Logic {
   //  * res0: Boolean = true
   //  */
   // def isPrime: Boolean = ???
+
+  /**
+   * P46 (**) Truth tables for logical expressions.
+   * Define functions and, or, nand, nor, xor, impl, and equ (for logical equivalence) which return true or false
+   * according to the result of their respective operations;
+   * e.g. and(A, B) is true if and only if both A and B are true.
+   *
+   * scala> and(true, true)
+   * res0: Boolean = true
+   *
+   * scala> xor(true. true)
+   * res1: Boolean = false
+   *
+   * A logical expression in two variables can then be written as an function of two variables,
+   * e.g: (a: Boolean, b: Boolean) => and(or(a, b), nand(a, b))
+   *
+   * Now, write a function called table2 which prints the truth table of a given logical expression in two variables.
+   *
+   * scala> table2((a: Boolean, b: Boolean) => and(a, or(a, b)))
+   * A     B     result
+   * true  true  true
+   * true  false true
+   * false true  false
+   * false false false
+   */
+  def and(a: Boolean, b: Boolean): Boolean = ???
+  def or(a: Boolean, b: Boolean): Boolean = ???
+  def nand(a: Boolean, b: Boolean): Boolean = ???
+  def nor(a: Boolean, b: Boolean): Boolean = ???
+  def xor(a: Boolean, b: Boolean): Boolean = ???
+  def impl(a: Boolean, b: Boolean): Boolean = ???
+  def equ(a: Boolean, b: Boolean): Boolean = ???
+  def table2(f: (Boolean, Boolean) => Boolean)(implicit out: OutputStream = Console.out): Unit = ???
 }
--- src/test/scala/problems/Problems03Spec.scala
+++ src/test/scala/problems/Problems03Spec.scala
@@ -4,9 +4,145 @@ import problems.logic._
 import org.specs2.mutable._
 
 class LogicAndCodesSpec extends Specification {
-  "S99Logic#example" should {
-    "???" in {
-      pending
+  "S99Logic->and(Boolean, Boolean)" should {
+    "returns true if (a, b) = (true, true)" in {
+      S99Logic.and(true, true) must beTrue
+    }
+
+    "returns false if (a, b) = (true, false)" in {
+      S99Logic.and(true, false) must beFalse
+    }
+
+    "returns false if (a, b) = (false, true)" in {
+      S99Logic.and(false, true) must beFalse
+    }
+
+    "returns false if (a, b) = (false, false)" in {
+      S99Logic.and(false, false) must beFalse
+    }
+  }
+
+  "S99Logic->or(Boolean, Boolean)" should {
+    "returns true if (a, b) = (true, true)" in {
+      S99Logic.or(true, true) must beTrue
+    }
+
+    "returns true if (a, b) = (true, false)" in {
+      S99Logic.or(true, false) must beTrue
+    }
+
+    "returns true if (a, b) = (false, true)" in {
+      S99Logic.or(false, true) must beTrue
+    }
+
+    "returns false if (a, b) = (false, false)" in {
+      S99Logic.or(false, false) must beFalse
+    }
+  }
+
+  "S99Logic->nand(Boolean, Boolean)" should {
+    "returns false if (a, b) = (true, true)" in {
+      S99Logic.nand(true, true) must beFalse
+    }
+
+    "returns true if (a, b) = (true, false)" in {
+      S99Logic.nand(true, false) must beTrue
+    }
+
+    "returns true if (a, b) = (false, true)" in {
+      S99Logic.nand(false, true) must beTrue
+    }
+
+    "returns true if (a, b) = (false, false)" in {
+      S99Logic.nand(false, false) must beTrue
+    }
+  }
+
+  "S99Logic->nor(Boolean, Boolean)" should {
+    "returns false if (a, b) = (true, true)" in {
+      S99Logic.nor(true, true) must beFalse
+    }
+
+    "returns false if (a, b) = (true, false)" in {
+      S99Logic.nor(true, false) must beFalse
+    }
+
+    "returns false if (a, b) = (false, true)" in {
+      S99Logic.nor(false, true) must beFalse
+    }
+
+    "returns true if (a, b) = (false, false)" in {
+      S99Logic.nor(false, false) must beTrue
+    }
+  }
+
+  "S99Logic->xor(Boolean, Boolean)" should {
+    "returns false if (a, b) = (true, true)" in {
+      S99Logic.xor(true, true) must beFalse
+    }
+
+    "returns true if (a, b) = (true, false)" in {
+      S99Logic.xor(true, false) must beTrue
+    }
+
+    "returns true if (a, b) = (false, true)" in {
+      S99Logic.xor(false, true) must beTrue
+    }
+
+    "returns false if (a, b) = (false, false)" in {
+      S99Logic.xor(false, false) must beFalse
+    }
+  }
+
+  "S99Logic->impl(Boolean, Boolean)" should {
+    "returns true if (a, b) = (true, true)" in {
+      S99Logic.impl(true, true) must beTrue
+    }
+
+    "returns false if (a, b) = (true, false)" in {
+      S99Logic.impl(true, false) must beFalse
+    }
+
+    "returns true if (a, b) = (false, true)" in {
+      S99Logic.impl(false, true) must beTrue
+    }
+
+    "returns true if (a, b) = (false, false)" in {
+      S99Logic.impl(false, false) must beTrue
+    }
+  }
+
+  "S99Logic->equ(Boolean, Boolean)" should {
+    "returns true if (a, b) = (true, true)" in {
+      S99Logic.equ(true, true) must beTrue
+    }
+
+    "returns false if (a, b) = (true, false)" in {
+      S99Logic.equ(true, false) must beFalse
+    }
+
+    "returns true if (a, b) = (false, true)" in {
+      S99Logic.equ(false, true) must beFalse
+    }
+
+    "returns true if (a, b) = (false, false)" in {
+      S99Logic.equ(false, false) must beTrue
+    }
+  }
+
+  "S99Logic->table2((Boolean, Boolean) => Boolean)" should {
+    "prints table [and(a, or(a, b))]" in {
+      implicit val out = new java.io.ByteArrayOutputStream()
+      val expected = """
+A     B     result
+true  true  true
+true  false true
+false true  false
+false false false
+      """.trim
+
+      S99Logic.table2((a: Boolean, b: Boolean) => a && (a || b))
+      out.toString.trim must beEqualTo(expected)
     }
   }
 }
