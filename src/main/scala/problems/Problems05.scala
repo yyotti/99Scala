82858e6e2319d0de386225d041966268cd82407a
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-09-01 08:57:06 +0900

    P70C の準備

--- src/main/scala/problems/Problems05.scala
+++ src/main/scala/problems/Problems05.scala
@@ -2,6 +2,15 @@ package problems.multiwaytree
 
 case class MTree[+T](value: T, children: List[MTree[T]]) {
   override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+
+  /**
+   * P70C (*) Count the nodes of a multiway tree.
+   * Write a method nodeCount which counts the nodes of a given multiway tree.
+   *
+   * scala> MTree('a', List(MTree('f'))).nodeCount
+   * res0: Int = 2
+   */
+  def nodeCount: Int = ???
 }
 
 object MTree {
--- src/test/scala/problems/Problems05Spec.scala
+++ src/test/scala/problems/Problems05Spec.scala
@@ -4,9 +4,33 @@ import problems.multiwaytree._
 import org.specs2.mutable._
 
 class MultiwayTreesSpec extends Specification {
-  "MTree#example" should {
-    "returns true" in {
-      true must beTrue
+  "MTree#nodeCount" should {
+    "returns 1 if this = MTree('a)" in {
+      MTree('a).nodeCount must beEqualTo(1)
+    }
+
+    "returns 2 if this = MTree('a, List(MTree('b)))" in {
+      MTree('a, List(MTree('b))).nodeCount must beEqualTo(2)
+    }
+
+    "returns 3 if this = MTree('a, List(MTree('b), MTree('c)))" in {
+      MTree('a, List(MTree('b), MTree('c))).nodeCount must beEqualTo(3)
+    }
+
+    "returns 4 if this = MTree('a, List(MTree('b), MTree('c), MTree('d)))" in {
+      MTree('a, List(MTree('b), MTree('c), MTree('d))).nodeCount must beEqualTo(4)
+    }
+
+    "returns 5 if this = MTree('a, List(MTree('b), MTree('c, List(MTree('e))), MTree('d)))" in {
+      MTree('a, List(MTree('b), MTree('c, List(MTree('e))), MTree('d))).nodeCount must beEqualTo(5)
+    }
+
+    "returns 6 if this = MTree('a, List(MTree('b), MTree('c, List(MTree('e))), MTree('d, List(MTree('f)))))" in {
+      MTree('a, List(MTree('b), MTree('c, List(MTree('e))), MTree('d, List(MTree('f))))).nodeCount must beEqualTo(6)
+    }
+
+    "returns 7 if this = MTree('a, List(MTree('b), MTree('c, List(MTree('e, MTree('g)))), MTree('d, List(MTree('f)))))" in {
+      MTree('a, List(MTree('b), MTree('c, List(MTree('e), MTree('g))), MTree('d, List(MTree('f))))).nodeCount must beEqualTo(7)
     }
   }
 }
