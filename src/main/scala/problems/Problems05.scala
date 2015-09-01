a7569c6a6667bb72470dba97296b9aa339f8c144
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-09-01 09:27:19 +0900

    P70 の準備

--- src/main/scala/problems/Problems05.scala
+++ src/main/scala/problems/Problems05.scala
@@ -11,8 +11,35 @@ case class MTree[+T](value: T, children: List[MTree[T]]) {
    * res0: Int = 2
    */
   def nodeCount: Int = 1 + children.map { _.nodeCount }.sum
+
+  /**
+   * P70 (**) Tree construction from a node string.
+   * Write the reverse function, and make it the toString method of MTree.
+   *
+   * scala> MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).toString
+   * res0: String = afg^^c^bd^e^^^
+   */
+  def toString2: String = ???
 }
 
 object MTree {
   def apply[A](value: A): MTree[A] = MTree(value, Nil)
+
+  /**
+   * P70 (**) Tree construction from a node string.
+   * We suppose that the nodes of a multiway tree contain single characters.
+   * In the depth-first order sequence of its nodes, a special character ^ has been inserted whenever, during the tree traversal,
+   * the move is a backtrack to the previous level.
+   * By this rule, the tree in the figure opposite is represented as:
+   *
+   * afg^^c^bd^e^^^
+   *
+   * Define the syntax of the string and write a function string2MTree to construct an MTree from a String.
+   *
+   * scala> MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).toString
+   * res0: String = afg^^c^bd^e^^^
+   */
+  import scala.language.implicitConversions
+  implicit def s2MT(s: String): MTree[Char] = string2MTree(s)
+  def string2MTree(s: String): MTree[Char] = ???
 }
--- src/test/scala/problems/Problems05Spec.scala
+++ src/test/scala/problems/Problems05Spec.scala
@@ -37,5 +37,76 @@ class MultiwayTreesSpec extends Specification {
       MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).nodeCount must beEqualTo(7)
     }
   }
+
+  "MTree#toString2" should {
+    """returns "a^" if this = MTree('a')""" in {
+      MTree('a').toString2 must beEqualTo("a^")
+    }
+
+    """returns "ab^^" if this = MTree('a', List(MTree('b')))""" in {
+      MTree('a', List(MTree('b'))).toString2 must beEqualTo("ab^^")
+    }
+
+    """returns "ab^c^^" if this = MTree('a', List(MTree('b'), MTree('c')))""" in {
+      MTree('a', List(MTree('b'), MTree('c'))).toString2 must beEqualTo("ab^c^^")
+    }
+
+    """returns "ab^c^d^^" if this = MTree('a', List(MTree('b'), MTree('c'), MTree('d')))""" in {
+      MTree('a', List(MTree('b'), MTree('c'), MTree('d'))).toString2 must beEqualTo("ab^c^d^^")
+    }
+
+    """returns "ab^ce^^d^^" if this = MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d')))""" in {
+      MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d'))).toString2 must beEqualTo("ab^ce^^d^^")
+    }
+
+    """returns "ab^ce^^df^^^" if this = MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d', List(MTree('f')))))""" in {
+      MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d', List(MTree('f'))))).toString2 must beEqualTo("ab^ce^^df^^^")
+    }
+
+    """returns "ab^ce^g^^df^^^" if this = MTree('a', List(MTree('b'), MTree('c', List(MTree('e'), MTree('g')))), MTree('d', List(MTree('f')))))""" in {
+      MTree('a', List(MTree('b'), MTree('c', List(MTree('e'), MTree('g'))), MTree('d', List(MTree('f'))))).toString2 must beEqualTo("ab^ce^g^^df^^^")
+    }
+
+    """returns "afg^^c^bd^e^^^" if this = MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e')))))""" in {
+      MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).toString2 must beEqualTo("afg^^c^bd^e^^^")
+    }
+  }
+
+  "MTree->string2MTree" should {
+    """throws IllegalArgumentException if this = "a"""" in {
+      MTree.string2MTree("a") must throwA[IllegalArgumentException]
+    }
+
+    """returns MTree('a') if this = "a^"""" in {
+      MTree.string2MTree("a^") must beEqualTo(MTree('a'))
+    }
+
+    """returns MTree('a', List(MTree('b'))) if this = "ab^^"""" in {
+      MTree.string2MTree("ab^^") must beEqualTo(MTree('a', List(MTree('b'))))
+    }
+
+    """returns MTree('a', List(MTree('b'), MTree('c'))) if this = "ab^c^^"""" in {
+      MTree.string2MTree("ab^c^^") must beEqualTo(MTree('a', List(MTree('b'), MTree('c'))))
+    }
+
+    """returns MTree('a', List(MTree('b'), MTree('c'), MTree('d'))) if this = "ab^c^d^^"""" in {
+      MTree.string2MTree("ab^c^d^^") must beEqualTo(MTree('a', List(MTree('b'), MTree('c'), MTree('d'))))
+    }
+
+    """returns MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d'))) if this = "ab^ce^^d^^"""" in {
+      MTree.string2MTree("ab^ce^^d^^") must beEqualTo(MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d'))))
+    }
+
+    """returns MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d', List(MTree('f'))))) if this = "ab^ce^^df^^^"""" in {
+      MTree.string2MTree("ab^ce^^df^^^") must beEqualTo(MTree('a', List(MTree('b'), MTree('c', List(MTree('e'))), MTree('d', List(MTree('f'))))))
+    }
+
+    """returns MTree('a', List(MTree('b'), MTree('c', List(MTree('e', MTree('g')))), MTree('d', List(MTree('f'))))) if this = "ab^ce^g^^df^^^"""" in {
+      MTree.string2MTree("ab^ce^g^^df^^^") must beEqualTo(MTree('a', List(MTree('b'), MTree('c', List(MTree('e'), MTree('g'))), MTree('d', List(MTree('f'))))))
+    }
+
+    """returns MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))) if this = "afg^^c^bd^e^^^"""" in {
+      MTree.string2MTree("afg^^c^bd^e^^^") must beEqualTo(MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))))
+    }
   }
 }
